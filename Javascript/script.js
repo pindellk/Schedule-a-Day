d24b7b5bf98d14e3cd1d49fe81259cab5d0388aa
pindellk <pindellk@protonmail.com>
2020-09-22 23:16:16 -0400

    Add script to html file, add execution notes to script file

---
+++ Javascript/script.js
@@ -0,0 +1,26 @@
+// Add date at top of page in <p> element with $("#currentDay")
+// Format: Wednesday, September 23rd
+
+// Save items into the page 
+    // SPECIFIC - Hook into div id $("#hour-9") 
+    // GENERAL - Hook into div class = $("time-block")
+
+// Append to DOM - not sure we need this
+// Final prepend to div class = $("container")
+
+// Save items to local storage when save button is hit
+    // BUTTON CLASS = $(".saveBtn") 
+    // or ICON CLASS = $(".fa-save")
+
+
+// If the time block is in the past: gray
+// Current: red
+// Future: green
+
+// use class $(".textarea") to modify colors
+
+// Gets hour - accepts 0-23
+    // moment().hour(Number);
+
+
+
