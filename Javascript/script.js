4976075333be7ac2bfa8d73e7804c2313ad75e5a
pindellk <pindellk@protonmail.com>
2020-09-23 23:46:48 -0400

    Update save button on click event, save entries to local storage and display on refresh

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,26 +1,34 @@
-// Add date at top of page in <p> element with $("#currentDay")
-// Format: Wednesday, September 23rd
 
-// Save items into the page 
-    // SPECIFIC - Hook into div id $("#hour-9") 
-    // GENERAL - Hook into div class = $("time-block")
-
-// Append to DOM - not sure we need this
-// Final prepend to div class = $("container")
-
-// Save items to local storage when save button is hit
-    // BUTTON CLASS = $(".saveBtn") 
-    // or ICON CLASS = $(".fa-save")
-
+// Load content from local storage
 
 // If the time block is in the past: gray
 // Current: red
 // Future: green
-
 // use class $(".textarea") to modify colors
 
-// Gets hour - accepts 0-23
-    // moment().hour(Number);
 
+// Display current date to top of schedule
+var currentDay = moment().format('dddd, MMMM Do');
+$("#currentDay").text = currentDay;
+$(".jumbotron").append(currentDay);
+
+$(document).ready(function () {
+
+    // Create event listener for save button icon
+    $(".saveBtn").on("click", function () {
+        var entry = $(this).siblings("textarea")[0];
+        var hour = $(this).parent();
+        var key = hour[0].id;
+        localStorage.setItem(key, $(entry).val().trim());
+    });
+
+    $.each(localStorage, function (key, value) {
+        $("#" + key).find("textarea").val(value);
+    });
+
+   $.each((moment().hour());
+
+
+});
 
 
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Schedule-a-Day
+JHU Boot Camp Homework 5
+
+Get organized and schedule your day with an hourly planner. Enter notes and events in the appropriate time slot and hit the floppy disc button on the right to save. Stay on task with color coding: gray blocks are in the past, the red block is current,  and green blocks are - no shock here - in the future.
+
+
+[Link to external site](https://pindellk.github.io/Schedule-a-Day/)
+
+
+![Image](./X.png)
+
